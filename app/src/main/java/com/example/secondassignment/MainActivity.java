d6823b0555682491851b6cdf8cc989c7d22abde0
shresthashreya4355 <shreyastha590@gmail.com>
2019-11-13 13:35:15 +0545

    tablelayout design

--- app/src/main/java/com/example/secondassignment/MainActivity.java
+++ app/src/main/java/com/example/secondassignment/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.secondassignment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editTextName, editTextDob, editTextPhone, editTextEmail, editTextImage;
+    RadioGroup radioGroupGender;
+    Spinner spinnerCountry;
+    Button buttonSubmit, buttonView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextName = findViewById(R.id.etName);
+        editTextDob = findViewById(R.id.etBirth);
+        editTextPhone = findViewById(R.id.etPhone);
+        editTextEmail = findViewById(R.id.etEmail);
+        editTextImage = findViewById(R.id.etImage);
+        radioGroupGender = findViewById(R.id.rgGender);
+        spinnerCountry = findViewById(R.id.spCountry);
+        buttonSubmit = findViewById(R.id.btnSubmit);
+        buttonView = findViewById(R.id.btnView);
     }
 }
